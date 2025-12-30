6b43c5e6131646ab30ed123c92c25be459b4cf96
Rizvan337 <rizvanichu337@gmail.com>
2025-12-30 20:31:44 +0400

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoute.js";
+dotenv.config();
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
